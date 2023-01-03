d0792995774c36bb47116631b6ed09d25e0ce185
Riotor Mathias <mathias.riotor@gmail.com>
2023-01-03 01:40:51 +0100

    Add favicon

--- public/themes/dijiteol/header.php
+++ public/themes/dijiteol/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="icon" type="image/png" href="<?= get_field('favicon', 'option') ? get_field('favicon', 'option')['url'] : '' ?>" />
     <?php wp_head(); ?>
 </head>
 
